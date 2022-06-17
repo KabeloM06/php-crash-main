7ff3750b13b5bb377b481b963e458108b3d604cb
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-06-17 15:08:20 +0200

    Initial commit

---
+++ _starter_files/01_output.php
@@ -0,0 +1,7 @@
+<?php
+/* -------- Output & Comments ------- */
+
+/*
+  A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
+*/
+echo 'hello12345678', "hello there", 145000, 100.5;
