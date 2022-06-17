9a14d0bf93a2ddbb914676264c782d83a8d677b0
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-06-17 18:42:55 +0200

    completed 04_conditionals

--- _starter_files/04_conditionals.php
+++ _starter_files/04_conditionals.php
@@ -10,7 +10,7 @@
   <= Less than or equal to
   >= Greater than or equal to
   == Equal to
-  === Identical to
+  === Identical to in value and type
   != Not equal to
   !== Not identical to
 */
@@ -24,12 +24,54 @@ if (condition) {
 }
 */
 
+$age = 20;
+
+if ($age >= 18) {
+  echo 'You are old enough to vote';
+} else {
+  echo'Sorry you are too young to vote';
+}
+
+$d = date("H"); // date() function formats date and time
+
+if ($d < 12) {
+  echo'Good morning';
+} elseif ($d < 18) {
+  echo 'Good afternoon';
+} else {
+  echo'Good evening';
+}
+
+$posts = ['First Post', 'Second Post', 'Last Post'];
+ if(!empty($posts)){
+  
+  echo $posts[1];
+ };
+
+
 /* -------- Ternary Operator -------- */
 /*
   The ternary operator is a shorthand if statement.
   Ternary Syntax:
     condition ? true : false;
 */
+echo !empty($posts) ? $posts[0] : 'No Posts';
+$last_post = !empty($posts) ? $posts[2] : 'No Posts';
+echo $last_post;
 
 
 /* -------- Switch Statements ------- */
+$favcolour = 'yellow';
+switch($favcolour) {
+  case 'red':
+    echo 'Your favorite colour is red';
+    break;
+  case 'yellow':
+    echo 'Your favorite colour is yellow';
+    break;
+  case 'blue':
+    echo 'Your favorite colour is blue';
+    break;
+  default:
+    echo 'Your favourite colour is not a primary colour';  
+}
