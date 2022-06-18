79a30528f43f3b61824902c392ef9159c1df5d3b
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-06-18 11:27:56 +0200

    completed 06_functions

--- _starter_files/06_functions.php
+++ _starter_files/06_functions.php
@@ -12,3 +12,29 @@
     // code to be executed
   }
 */
+
+function whatIsMyName($name) {
+  echo "Your name is $name";
+};
+
+whatIsMyName('Kabelo');
+
+function sumOfNumbers($n1, $n2){
+  return $sum = $n1 + $n2;
+  
+};
+echo '<br>';
+echo sumOfNumbers(25, 34);
+
+//anonymous functions
+$subtract = function($n1, $n2){
+  return $n1 - $n2;
+}; // This needs a ; at the end 
+echo '<br>';
+echo $subtract(80, 45);
+echo '<br>';
+// or
+
+$multiply = fn($n1, $n2) => $n1 * $n2;
+echo "Multiply 3 * 3 = {$multiply(3,3)}";
+echo '<br>';
